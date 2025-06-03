bdca75c39229b50f609334ff47c2e4fc544b8ff1
dtec8 <62524773+DTec-Core@users.noreply.github.com>
2025-06-02 23:01:56 -0400

    Update test_signer.py

--- tests/test_signer.py
+++ tests/test_signer.py
@@ -4,11 +4,5 @@ import os
 
 def test_sign_file():
     sample_file = "sample.txt"
-    key_file = "ideal-lattice_private.pem"
-
-    with open(sample_file, "w") as f:
-        f.write("This is a test document.")
-
-    signer.sign_file(sample_file, key_file)
 
     assert os.path.exists(sample_file + ".idtec-signature")
