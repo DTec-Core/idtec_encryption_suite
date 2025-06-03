eba0398694bffb5453793a8661f864d59fc07de7
dtec8 <62524773+DTec-Core@users.noreply.github.com>
2025-06-02 22:47:31 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -7,12 +7,6 @@ def main():
     pass
 
 @main.command()
-@click.option('--file', required=True, help='File to sign')
-@click.option('--key', required=True, help='Private key file')
-def sign(file, key):
-    signer.sign_file(file, key)
-
-@main.command()
 @click.option('--message', required=True, help='Message to encrypt')
 @click.option('--key', required=True, help='256-bit key')
 def encrypt(message, key):
