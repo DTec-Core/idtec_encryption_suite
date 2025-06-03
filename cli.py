e513c3d1df8d2554c652a2222766bffc2c786575
dtec8 <62524773+DTec-Core@users.noreply.github.com>
2025-06-02 22:57:05 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -2,12 +2,7 @@
 import click
 from idtec_core import signer, threefish
 
-@click.group()
-def main():
-    pass
-
 @main.command()
-@click.option('--message', required=True, help='Message to encrypt')
 @click.option('--key', required=True, help='256-bit key')
 def encrypt(message, key):
     result = threefish.encrypt_message(message, key)
